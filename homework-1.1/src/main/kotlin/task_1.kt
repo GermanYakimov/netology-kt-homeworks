cac8b1f9d34ef046189a62df7be99af114ef46e9
German Yakimov <german13yakimov@gmail.com>
2019-08-29 13:16:10 +0600

    Solve first task.

---
+++ homework-1.1/src/main/kotlin/task_1.kt
@@ -0,0 +1,17 @@
+fun main() {
+    println(calculateFee(amount = 200, total = 11_000))
+    println(calculateFee(amount = 500, total = 800, exclusive = true))
+    println(calculateFee(amount = 300, total = 60_000))
+}
+
+fun calculateFee(amount: Int, total: Int, exclusive: Boolean = false): Double {
+    val percent: Double = (if (exclusive) -0.05 else 0.0) + when {
+        (total in 0..1000) -> 0.3
+        (total in 1001..10_000) -> 0.25
+        (total in 10_001..50_000) -> 0.2
+        else -> 0.15
+    }
+
+    return amount * percent // можно было сократить все до одного выражения, просто сразу вернув результат умножения
+                            // amount на percent, который считается выше, но, на мой взгляд, это уменьшит читабельность
+}
