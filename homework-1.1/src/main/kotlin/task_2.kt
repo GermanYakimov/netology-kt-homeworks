8c4fe958523dd9b8cdd8752521e21d008042a6b3
German Yakimov <german13yakimov@gmail.com>
2019-08-29 14:38:10 +0600

    Solve second task.

--- homework-1.1/src/main/kotlin/task_2.kt
+++ homework-1.1/src/main/kotlin/task_2.kt
@@ -1,3 +1,18 @@
 fun main() {
+    println(massIndexVerdict(bodyMassIndex(70.0, 1.82)))
+}
 
+fun massIndexVerdict(index: Double): String = when {
+    (index <= 16) -> "pronounced deficit of body mass"
+    (index > 16 && index <= 18.5) -> "deficit of body mass"
+    (index > 18.5 && index <= 25) -> "normal"
+    (index > 25 && index <= 30) -> "excess body mass"
+    (index > 30 && index <= 35) -> "obesity"
+    (index > 35 && index <= 40) -> "severe obesity"
+    else -> "very severe obesity"
+}
+
+
+fun bodyMassIndex(mass: Double, height: Double): Double {
+    return mass / (height * height)
 }
