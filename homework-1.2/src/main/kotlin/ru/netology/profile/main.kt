7ee02c3311a890402a0a7d514a5d1958f9b45ddb
German Yakimov <german13yakimov@gmail.com>
2019-09-08 21:37:38 +0700

    Solve first task

--- homework-1.2/src/main/kotlin/ru/netology/profile/main.kt
+++ homework-1.2/src/main/kotlin/ru/netology/profile/main.kt
@@ -1,3 +1,14 @@
 package ru.netology.profile
 
+import ru.netology.profile.model.Profile
 
+fun main() {
+    val profile = Profile(1, "something", "Ivan", "Ivanov", "Hello", "")
+
+    println(profile.id)
+    println(profile.login)
+    println(profile.name)
+    println(profile.surname)
+    println(profile.status)
+    println(profile.fullname)
+}
--- homework-1.2/src/main/kotlin/ru/netology/profile/model/Profile.kt
+++ homework-1.2/src/main/kotlin/ru/netology/profile/model/Profile.kt
@@ -1,4 +1,11 @@
 package ru.netology.profile.model
 
-class Profile {
+class Profile (val id: Int,
+               val login: String,
+               var name: String,
+               var surname: String,
+               var status: String,
+               var avatar: String) {
+
+    var fullname: String = "$name $surname"
 }
