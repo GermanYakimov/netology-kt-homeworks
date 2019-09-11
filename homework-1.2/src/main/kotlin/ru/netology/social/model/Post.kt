1187afde67cb75f25c7e684b45d00a21ab35b0d7
German Yakimov <german13yakimov@gmail.com>
2019-09-11 18:59:30 +0700

    Solve third task.

--- homework-1.2/src/main/kotlin/ru/netology/social/model/Post.kt
+++ homework-1.2/src/main/kotlin/ru/netology/social/model/Post.kt
@@ -1,4 +1,15 @@
 package ru.netology.social.model
 
 class Post {
+    val id: Int = 0
+    lateinit var text: String
+    val date: Int = 0
+    val ownerId: Int = 0
+    var comments = object {}
+    var likes = object {}
+    var reposts = object {}
+    var views: Int = 0
+    var geo = null
+    var isPinned: Boolean = false
+    var createdBy: Int = 0
 }
