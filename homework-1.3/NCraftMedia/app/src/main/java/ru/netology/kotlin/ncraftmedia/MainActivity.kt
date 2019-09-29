7acbc4c208770f042024dd95e92c29ffac7a3a9c
German Yakimov <german13yakimov@gmail.com>
2019-09-29 21:02:23 +0700

    Add code from lecture.

---
+++ homework-1.3/NCraftMedia/app/src/main/java/ru/netology/kotlin/ncraftmedia/MainActivity.kt
@@ -0,0 +1,24 @@
+package ru.netology.kotlin.ncraftmedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.netology.kotlin.ncraftmedia.ru.netology.kotlin.dto.Post
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val post = Post(1, "Vasya", "First post in our network!", "20 august 2019")
+        createdTv.text = post.created
+        contentTv.text = post.content
+
+        post.likedByMe = true
+
+        if(post.likedByMe){
+            likeBtn.setBackgroundResource(R.drawable.ic_favorite_black_24dp)
+        }
+    }
+}
